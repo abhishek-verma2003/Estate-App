003ee94064f2a4eff18555ef159c9da66cbc0870
abhishek-verma2003 <abhishekvermacivil18@gmail.com>
2025-02-03 01:38:11 +0530

    changed file

--- api/routes/test.route.js
+++ api/routes/test.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { shouldBeAdmin, shouldBeLoggedIn } from "../controllers/test.controller.js";
-import { verifyToken } from "../middleware/verifytoken.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router()
 
